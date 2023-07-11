2f716788ecc176a21ee001b81f7568a8673730e7
enmanuelbayen <enmanuel0495@gmail.com>
2023-07-11 17:00:52 -0400

    add new values and transactions queries for animals

--- queries.sql
+++ queries.sql
@@ -8,3 +8,40 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+BEGIN;
+UPDATE animals
+SET species='unspecified'
+WHERE species IS NULL;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
+
+-- delete
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+SAVEPOINT SP1;
+DELETE FROM animals WHERE date_of_birth > '2022/01/01';
+SAVEPOINT SP2;
+UPDATE animals SET weight_kg= weight_kg*-1;
+SAVEPOINT SP3;
+ROLLBACK TO SP2;
+UPDATE animals SET weight_kg= weight_kg*-1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
